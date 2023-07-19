d47a330758233222ffe60ffbee65e2c1b1b45a07
Steven <116656099+StevenK-293@users.noreply.github.com>
2023-07-19 09:45:11 -0400

    Update script.js

--- WaifuGenerator4/script.js
+++ WaifuGenerator4/script.js
@@ -8,25 +8,16 @@ async function fetchTags() {
   try {
     const response = await fetch('https://api.waifu.im/tags');
     if (!response.ok) {
-      throw new Error('Tags request failed with status code: ' + response.status);
+      throw new Error('Failed to fetch tags: ' + response.status);
     }
     const tagsData = await response.json();
     const apiTags = tagsData.tags;
 
     const tagDropdown = document.getElementById('tag-dropdown');
     const modeDropdown = document.getElementById('mode-dropdown');
-    const fragment = document.createDocumentFragment();
-
     const selectedTags = tags[modeDropdown.value];
 
-    for (const tag of selectedTags) {
-      const option = document.createElement('option');
-      option.value = tag;
-      option.textContent = tag;
-      fragment.appendChild(option);
-    }
-
-    tagDropdown.appendChild(fragment);
+    updateTagDropdown(selectedTags);
     tagDropdown.addEventListener('change', fetchImageByTag);
     modeDropdown.addEventListener('change', updateTagsDropdown);
   } catch (error) {
@@ -38,64 +29,102 @@ async function fetchImageByTag() {
   try {
     const selectedTag = document.getElementById('tag-dropdown').value;
     const apiUrlWithParams = `${apiUrl}?included_tags=${selectedTag}`;
+
+    // Show loading state
+    showLoadingState();
+
     const response = await fetch(apiUrlWithParams);
 
     if (!response.ok) {
-      throw new Error('Image request failed with status code: ' + response.status);
+      throw new Error('Failed to fetch image: ' + response.status);
     }
 
     const imageData = await response.json();
     const image = imageData.images[0]; // Assuming there is only one image in the response
 
-    const resultDiv = document.getElementById('result');
-    const imageElement = resultDiv.querySelector('img');
-    const sourceLink = resultDiv.querySelector('a');
-    const tagsContainer = resultDiv.querySelector('.tags-container');
-
-    imageElement.src = image.url;
-    sourceLink.href = image.source;
-
-    while (tagsContainer.firstChild) {
-      tagsContainer.removeChild(tagsContainer.firstChild);
-    }
-
-    for (const tag of image.tags) {
-      const tagElement = document.createElement('span');
-      tagElement.classList.add('tag');
-      tagElement.textContent = tag.name;
-      tagsContainer.appendChild(tagElement);
-    }
+    updateImageElement(image);
+    updateTagsContainer(image.tags);
   } catch (error) {
     console.error('An error occurred:', error.message);
+  } finally {
+    // Hide loading state
+    hideLoadingState();
   }
 }
 
-function updateTagsDropdown() {
+function updateTagDropdown(selectedTags) {
   const tagDropdown = document.getElementById('tag-dropdown');
-  const modeDropdown = document.getElementById('mode-dropdown');
-  const selectedTags = tags[modeDropdown.value];
 
-  // Clear existing options
-  while (tagDropdown.firstChild) {
-    tagDropdown.removeChild(tagDropdown.firstChild);
-  }
+  clearDropdownOptions(tagDropdown);
 
-  // Create and append options for selected tags
   const fragment = document.createDocumentFragment();
-
   for (const tag of selectedTags) {
-    const option = document.createElement('option');
-    option.value = tag;
-    option.textContent = tag;
+    const option = createOptionElement(tag);
     fragment.appendChild(option);
   }
 
   tagDropdown.appendChild(fragment);
+}
+
+function updateImageElement(image) {
+  const resultDiv = document.getElementById('result');
+  const imageElement = resultDiv.querySelector('img');
+  const sourceLink = resultDiv.querySelector('a');
+
+  imageElement.src = image.url;
+  sourceLink.href = image.source;
+}
+
+function updateTagsContainer(tags) {
+  const tagsContainer = document.querySelector('.tags-container');
+  while (tagsContainer.firstChild) {
+    tagsContainer.removeChild(tagsContainer.firstChild);
+  }
+
+  for (const tag of tags) {
+    const tagElement = createTagElement(tag.name);
+    tagsContainer.appendChild(tagElement);
+  }
+}
+
+function clearDropdownOptions(dropdown) {
+  while (dropdown.firstChild) {
+    dropdown.removeChild(dropdown.firstChild);
+  }
+}
+
+function createOptionElement(tag) {
+  const option = document.createElement('option');
+  option.value = tag;
+  option.textContent = tag;
+  return option;
+}
+
+function createTagElement(tagName) {
+  const tagElement = document.createElement('span');
+  tagElement.classList.add('tag');
+  tagElement.textContent = tagName;
+  return tagElement;
+}
+
+function showLoadingState() {
+  const refreshButton = document.getElementById('refresh-button');
+  refreshButton.disabled = true;
+  refreshButton.textContent = 'Loading...';
+}
+
+function hideLoadingState() {
+  const refreshButton = document.getElementById('refresh-button');
+  refreshButton.disabled = false;
+  refreshButton.textContent = 'Refresh';
+}
+
+function handleRefreshButtonClick() {
   fetchImageByTag();
 }
 
+const refreshButton = document.getElementById('refresh-button');
+refreshButton.addEventListener('click', handleRefreshButtonClick);
+
 fetchTags();
 fetchImageByTag();
-
-const refreshButton = document.getElementById('refresh-button');
-refreshButton.addEventListener('click', fetchImageByTag);
